b30d1daba72c0ebad7e67a084a68403b9880ff36
andreas-oberheim <andreas.oberheim@gmail.com>
2016-08-18 12:03:27 +0200

    Add files via upload

---
+++ src/main/kotlin/org/universal_markup_converter/api/Convert.kt
@@ -0,0 +1,61 @@
+/*
+ * MIT License
+ * Copyright (c) 2016 Andreas M. Oberheim
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.universal_markup_converter.api
+
+class Convert(val inputType: InputType,
+			  val outputType: OutputType,
+			  val fromFile: String,
+			  val toFile: String) {
+
+	companion object {
+		fun from(from: InputType) = Convert(from, OutputType.NONE, "", "")
+	}
+
+	fun to(to: OutputType) = Convert(inputType, to, fromFile, toFile)
+	fun fromFile(file: String) = Convert(inputType, outputType, file, toFile)
+	fun toFile(file: String) = Convert(inputType, outputType, fromFile, file).convert()
+
+	private fun convert() {
+		println("convert")
+		when (inputType) {
+			InputType.MARKDOWN -> {
+				println("MarkdownConverter")
+				// val conv = MarkdownConverter(outputType, fromFile, toFile)
+				// conv.convert()
+				println("after MarkdownConverter.convert")
+			}
+			InputType.ASCIIDOC -> {
+				println("AsciiDocConverter")
+				val conv = AsciiDocConverter(outputType, fromFile, toFile)
+				conv.convert()
+				println("after conv.convert")
+			}
+			InputType.RESTRUCTURED_TEXT -> throw Exception("unknown input type $inputType")
+			InputType.ORG_MODE -> throw Exception("unknown input type $inputType")
+			InputType.MEDIAWIKI -> throw Exception("unknown input type $inputType")
+			else -> throw Exception("unknown input type $inputType")
+		}
+	}
+
+}
+
